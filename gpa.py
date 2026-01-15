303638347aa09e2315243999c50d4a6fcd76f381
陳聖翰 <alexshenhan@gmail.com>
2026-01-15 18:51:59 +0800

    GPA計算機第一版本(讀取excel，支援4.0 / 4.3制)

---
+++ gpa.py
@@ -0,0 +1,72 @@
+import pandas as pd
+
+#分數(score)轉換成等第(grade)的邏輯
+#4.0制度
+def score_to_point4(score):
+    if pd.isna(score):     #如果掃到的分數是空的就噴None
+        return None
+    if score >= 80:
+        return 4
+    elif 80 > score >= 70:
+        return 3
+    elif 70 > score >= 60:
+        return 2
+    elif 60 > score >= 50:
+        return 1
+    else:
+        return 0
+
+#4.3制度
+def score_to_point43(score):
+    if pd.isna(score):     #如果掃到的分數是空的 就噴None
+        return None
+    if 90 <= score:
+        return 4.3
+    elif 90 > score >= 85:
+        return 4.0
+    elif 85 > score >= 80:
+        return 3.7
+    elif 80 > score >= 77:
+        return 3.3
+    elif 77 > score >= 73:
+        return 3.0
+    elif 73 > score >= 70:
+        return 2.7
+    elif 70 > score >= 67:
+        return 2.3
+    elif 67 > score >= 63:
+        return 2.0
+    elif 63 > score >= 60:
+        return 1.7
+    else:
+        return 0
+
+#GPA計算 兩制度共用 : sigma(point * credit) / sigma(credit)
+def GPA(df, funct, term = None):
+
+    mask = ( 
+        (df["count_gpa"] == 1) & 
+        (~df["score"].isna()) &
+        ((df["term"] == term) if term is not None else True) 
+        )
+    df = df.loc[mask].copy()     #為了避免panda不清楚要操作view或df本體 所以乾脆直接複製一份出來
+    #loc : 直接用標籤來定位欄與列 (可以搭配mask 上面是在定位mask那幾列並複製)
+
+
+    df["point"] = df["score"].apply(funct)     #把成績轉換成點數
+
+    point_sum = (df["point"] * df["credit"]).sum()     #point * credict
+
+    gpa = round(point_sum / (df["credit"].sum()),2)     #計算GPA
+
+    return gpa 
+
+#主程式
+df = pd.read_excel("grade.xlsx")
+
+print(GPA(df, score_to_point4))
+print(GPA(df, score_to_point43))
+print(GPA(df, score_to_point4, "2024-1"))
+print(GPA(df, score_to_point4, "2024-2"))
+print(GPA(df, score_to_point43, "2024-1"))
+print(GPA(df, score_to_point43, "2024-2"))
